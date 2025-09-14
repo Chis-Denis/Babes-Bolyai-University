20c240b43b7bbf6358aff5d7d2d8732bfba813f9
Chis-Denis <denis20chis@gmail.com>
2025-09-14 14:21:20 +0300

    .

--- Semester 4/Web Programming/Assignments/A7 idk daca mere asta/api/reset_password.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'config.php';
-$newPassword = password_hash('newpassword123', PASSWORD_DEFAULT);
-$conn->query("UPDATE users SET password = '$newPassword' WHERE username = 'testuser'");
-echo 'Password updated!'; 
